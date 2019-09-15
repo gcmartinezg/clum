870ae1dc23897787f5dc97fd470075e14cf1ee24
German Martinez <germancmartinez@hotmail.com>
2019-09-14 22:06:15 -0500

    Commit #2: Correccion Cross-Origin Usuario

--- src/main/java/co/edu/usbcali/clum/controller/UsuarioRestController.java
+++ src/main/java/co/edu/usbcali/clum/controller/UsuarioRestController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin("*")
 public class UsuarioRestController {
     private static final Logger log = LoggerFactory.getLogger(UsuarioRestController.class);
     @Autowired
